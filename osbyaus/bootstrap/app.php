410f231d254ee8cc4665feee62d3ad3c02fe5639
akhtarrahmani <168260752+AkhtarRehmani@users.noreply.github.com>
2025-11-05 15:22:51 +0500

    set sanctum in app

--- osbyaus/bootstrap/app.php
+++ osbyaus/bootstrap/app.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
