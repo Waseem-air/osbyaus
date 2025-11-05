5f34c8de3e7e134e8394974727dbd0b52a1fe81b
akhtarrahmani <168260752+AkhtarRehmani@users.noreply.github.com>
2025-11-05 15:20:54 +0500

    install laravel breeze

--- osbyaus/bootstrap/app.php
+++ osbyaus/bootstrap/app.php
@@ -11,7 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Sanctum & session middleware for API auth
+        $middleware->api(prepend: [
+            EnsureFrontendRequestsAreStateful::class,
+            HandleCors::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            SubstituteBindings::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
