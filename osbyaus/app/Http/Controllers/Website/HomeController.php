39d2fa4d62e92b7388bedb43c8fcc17aca18d693
Muhammad Hassan <66895084+MuhammadHassandeveloper@users.noreply.github.com>
2025-11-17 09:51:29 +0500

    set home query

--- osbyaus/app/Http/Controllers/Website/HomeController.php
+++ osbyaus/app/Http/Controllers/Website/HomeController.php
@@ -11,9 +11,25 @@ class HomeController extends Controller
 
     public function home()
     {
-        $products = Product::with(['images','categories','colors','sizes','variants'])
-            ->popularThisWeek()->take(8)->get();
+        // Fetch popular products this week
+        $products = Product::with(['images', 'categories', 'colors', 'sizes', 'variants'])
+            ->popularThisWeek()
+            ->take(8)
+            ->get();
+
+        // If less than 8, fill with random products
+        if ($products->count() < 8) {
+            $remaining = 8 - $products->count();
+            $randomProducts = Product::with(['images', 'categories', 'colors', 'sizes', 'variants'])
+                ->whereNotIn('id', $products->pluck('id'))
+                ->inRandomOrder()
+                ->take($remaining)
+                ->get();
+
+            $products = $products->merge($randomProducts);
+        }
         return view('website.index', compact('products'));
     }
+
 
 }
